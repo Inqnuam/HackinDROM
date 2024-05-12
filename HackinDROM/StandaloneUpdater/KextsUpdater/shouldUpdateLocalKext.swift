d5a41d5c1905b41defa2d99661a5fdf3c0fd3f32
Inqnuam <vvanderlian@gmail.com>
2024-05-12 22:54:49 +0200

    fix: add description about BT API usage in info.plist
    fix: manually parse kernel extension malformed version tags when automatic parsing fails

--- HackinDROM/StandaloneUpdater/KextsUpdater/shouldUpdateLocalKext.swift
+++ HackinDROM/StandaloneUpdater/KextsUpdater/shouldUpdateLocalKext.swift
@@ -9,17 +9,55 @@
 import Foundation
 import Version
 func shouldUpdateLocalKext(_ kextInfo: GitHubInfo) -> String? {
- 
-   
-    
     guard let versions = getGitReleasesVersions(kextInfo.owner, kextInfo.repo, true) else {return  nil}
     
-    let remoteVersion = Version(tolerant: versions.first!)!
+    let version = versions.first!
+    var remoteVersion = Version(tolerant: version)
+    
+    
+    if remoteVersion == nil {
+        // trying to parse version manually, example: "0.7.2f1"
+        var rawVersionComponents: [String] = []
+        
+        for v in version.components(separatedBy:  ".") {
+            
+            if Int(v) != nil  {
+                rawVersionComponents.append(v)
+            } else {
+                
+                var versionComponent = ""
+                
+                for dirtyComponent in Array(v) {
+                    let dirtyString = String(dirtyComponent)
+                    if Int(dirtyString) == nil {
+                        break
+                    }
+                    versionComponent += dirtyString
+                }
+                
+                if !versionComponent.isEmpty {
+                    rawVersionComponents.append(versionComponent)
+                }
+                
+                break
+            }
+            
+        }
+        
+        remoteVersion = Version(tolerant: rawVersionComponents.joined(separator: "."))
+        
+    }
+    
+    if remoteVersion == nil {
+        return nil
+    }
+    
+    
     let localKextPath = latestFolder + "/\(kextInfo.repo)/\(kextInfo.name)"
     
     let localVersion = getKextVersionFrom(path: localKextPath)
     
-    if remoteVersion > localVersion {
+    if remoteVersion! > localVersion {
         return versions.first!
     } else {
         return nil
